317bc61bdb54ba620c353a5e8935e77b64e47f7a
BerkaJakub <berkajak@gmail.com>
2017-05-13 17:01:01 +0200

    pp

--- Perpetuum/src/pages/question-create/question-create.ts
+++ Perpetuum/src/pages/question-create/question-create.ts
@@ -1,48 +1,26 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuestionCreate page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+
+
 @Component({
   selector: 'page-question-create',
   templateUrl: 'question-create.html',
 })
 export class QuestionCreate {
-  questionList: FirebaseListObservable<any>;
+  
   answerList = ['Odpověď 1', 'Odpověď 2', 'Odpověď 3', 'Odpověď 4'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFire) {
-    this.questionList = af.database.list('/questions');
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuestionCreate');
-  }
+  
 
   customTrackBy(index: number, obj: any): any {
     return index;
   }
 
-  createQuestion(name, answerList, fromDate, toDate){
-    this.questionList.push(
-      {
-        creatorID: 1,
-        name: name,
-        answerList: answerList,
-        fromDate: fromDate,
-        toDate: toDate
-      }).then( newQuestion =>{
-        this.navCtrl.pop();
-      }, error => {
-        console.log(error);
-      });
-    
-  }
+  
 
 }
