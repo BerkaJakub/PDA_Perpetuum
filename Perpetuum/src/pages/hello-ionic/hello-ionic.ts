6ac345b1b6ab90c3f7fd6b628d862aa2107e12bb
BerkaJakub <berkajak@gmail.com>
2017-05-13 17:02:29 +0200

    kk

--- Perpetuum/src/pages/hello-ionic/hello-ionic.ts
+++ Perpetuum/src/pages/hello-ionic/hello-ionic.ts
@@ -1,34 +1,19 @@
 import { Component } from '@angular/core';
-//import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
-import { NavController, NavParams } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {QuestionCreate} from '../question-create/question-create';
-
-
-const questionsData = [{
-  title: 'Kdo1?',
- 
-  id: 0
-},{
-  title: 'Kdo2?',
-  
-  id: 1
-},{
-  title: 'Kdo3?',
-  
-  id: 2
-},]
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  questions: any;
+  questions: FirebaseObjectObservable<any>;
   
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //this.questions = angFire.database.object('/questions');
-    this.questions = questionsData;
+  constructor(public navCtrl: NavController, public navParams: NavParams, angFire: AngularFire) {
+    this.questions = angFire.database.object('/questions');
+    
     
   }
   
