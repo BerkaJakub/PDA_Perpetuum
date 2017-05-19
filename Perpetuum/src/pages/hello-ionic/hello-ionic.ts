393940c0869fa29e4d6e53e4da5963db6d9240c7
berkajak <berkajak@fel.cvut.cz>
2017-05-19 19:46:03 +0200

    Uprava odpovidani, procenta

--- Perpetuum/src/pages/hello-ionic/hello-ionic.ts
+++ Perpetuum/src/pages/hello-ionic/hello-ionic.ts
@@ -23,35 +23,39 @@ export class HelloIonicPage {
   answersArray: Array<string>;
   answer: FirebaseObjectObservable<any>;
   questionsAll: FirebaseObjectObservable<any>;
+  
 
   counter: number;
   numberOfQuestions: number;
   title: string;
   likes: number;
   dislikes: number;
+  answeredFlag: boolean;
+  answersNumbers: Array<any>;
+  selectedAnswer: number;
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public angFire: AngularFire) {
 
     this.counter = 0;
+    this.answeredFlag = false;
     this.questionsAll = angFire.database.object('/question');
     this.questions = angFire.database.object('/question/' + this.counter);
+    this.answersNumbers = [];
     this.questions.subscribe(question => {
       this.title = question.title;
       this.likes = question.likes;
       this.dislikes = question.dislikes;
     })
-    this.numberOfQuestions = 0;
+    
     this.answers = angFire.database.object('/question/' + this.counter + '/answers/');
 
     // Get number of questions
     this.questionsAll.subscribe(question =>{
+      this.numberOfQuestions = 0;
       question.forEach(q =>{this.numberOfQuestions++;});
     });
 
-    
-
-  
 
   }
 
@@ -59,7 +63,7 @@ export class HelloIonicPage {
 
   increment() {
 
-    if (this.counter == (this.numberOfQuestions-1)) {
+    if (this.counter >= (this.numberOfQuestions-1)) {
       this.counter = 0;
     } else {
       this.counter++;
@@ -73,6 +77,7 @@ export class HelloIonicPage {
 
     this.answers = this.angFire.database.object('/question/' + this.counter + '/answers/');
     console.log(this.counter);
+    this.answeredFlag = false;
 
   }
 
@@ -93,7 +98,8 @@ export class HelloIonicPage {
 
   submitAnswer(index) {
     console.log("Odpovezeno na: ", index);
-
+    this.selectedAnswer = index;
+    
     this.answer = this.angFire.database.object('/question/' + this.counter + '/answersNumbers/');
     let value;
     this.answer.subscribe(answers => {
@@ -101,11 +107,32 @@ export class HelloIonicPage {
       value = answers[index];
 
     });
-    console.log(value++);
+    value++;
 
-    this.answer.$ref.child(index).set(value);
+    let numberOfAnswers : any
+    this.questions = this.angFire.database.object('/question/' + this.counter);
+    this.questions.subscribe(question => {
+        numberOfAnswers = question.numAnswers;
+    });
+    numberOfAnswers++;
+    console.log(numberOfAnswers);
+    //this.questions.$ref.child('/answersNumbers/').child(index).set(value);// inkrementace poctu odpovedi pro konkretni otazku
+    // inkrementace poctu odpovedi celkove
+    this.questions.$ref.child('/').on('value', function(question){
+             question.ref.child('numAnswers').set(numberOfAnswers);
+             question.ref.child('/answersNumbers/').child(index).set(value);
+    });
 
-    this.increment();
+    // Vsechny pocty odpovedi do pole
+    this.answer.subscribe(answer => {
+      answer.forEach((value, index) =>{
+        var percent = Math.round((value / numberOfAnswers) * 100); 
+        this.answersNumbers[index] = percent;
+      });
+    });
+    
+    this.answeredFlag = true;
+
   }
 
 
